deb7816594dd315591d5dc1240116c1c2dd4f19d
CORP-AD\w191003 <mehmet.yagci@wordline.com>
2025-03-16 08:49:43 +0300

    Add project files.

---
+++ App.API/Program.cs
@@ -0,0 +1,40 @@
+﻿using App.Repository;
+using App.Repository.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddRepositories(builder.Configuration);
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//{
+//    // Eski kullanım şekli options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+
+//    var connectionString = builder.Configuration.
+//    GetSection(ConnectionStringOption.Key).Get<ConnectionStringOption>();
+
+//    options.UseSqlServer(connectionString!.SqlServer);
+//}); 
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ App.Service/ServiceAssembly.cs
@@ -0,0 +1,3 @@
+﻿namespace App.Service;
+
+public struct ServiceAssembly;
