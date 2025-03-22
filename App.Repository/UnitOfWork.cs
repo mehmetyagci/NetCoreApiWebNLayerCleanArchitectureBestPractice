7398a4abce22c37d7561a9bbfa7e67830280b2aa
CORP-AD\w191003 <mehmet.yagci@wordline.com>
2025-03-22 03:23:24 +0300

    asd

---
+++ App.Repository/UnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Repository;
+
+public class UnitOfWork(AppDbContext context) : IUnitOfWork
+{
+    public Task<int> SaveChangesAsync() => context.SaveChangesAsync();
+}
---
+++ App.Service/ServiceResult.cs
@@ -0,0 +1,25 @@
+﻿namespace App.Service;
+
+public class ServiceResult<T>
+{
+    /// <summary>
+    /// Başarılı olduğunda Data dolucak
+    /// </summary>
+    public T? Data { get; set; }
+
+    /// <summary>
+    /// Başarısız olduğunda hata mesajı dönecek
+    /// </summary>
+    public List<string>? ErrorMessage { get; set; }
+
+    public bool IsSuccess => ErrorMessage == null || ErrorMessage.Count == 0;
+    //public bool IsSuccess
+    //{
+    //    get
+    //    {
+    //        return ErrorMessage == null || ErrorMessage.Count == 0;
+    //    }
+    //}
+
+    public bool IsFail => !IsSuccess;
+}
