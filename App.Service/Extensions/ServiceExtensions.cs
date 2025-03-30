949c100ede4af1223f9f8994965107a37ccb70d8
CORP-AD\w191003 <mehmet.yagci@wordline.com>
2025-03-30 08:21:19 +0300

    20. Model Validation -2

--- App.Service/Extensions/ServiceExtensions.cs
+++ App.Service/Extensions/ServiceExtensions.cs
@@ -1,4 +1,7 @@
-﻿using App.Service.Products;
+﻿using System.Reflection;
+using App.Service.Products;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,6 +12,9 @@ public static class ServiceExtensions
     public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddScoped<IProductService, ProductService>();
+
+        services.AddFluentValidationAutoValidation();
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         return services;
     }
 }
---
+++ App.Service/Products/CreateProductRequestValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+
+namespace App.Service.Products;
+
+public class CreateProductRequestValidator : AbstractValidator<CreateProductRequest>
+{
+    public CreateProductRequestValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotNull().WithMessage("Ürün ismi gereklidir.")
+            .NotEmpty().WithMessage("Ürün ismi gereklidir.")
+            .Length(3,10).WithMessage("Ürün ismi 3 ile 10 karakter arasında olmalıdır.");
+
+        // price validation
+        RuleFor(x => x.Price)
+            .GreaterThan(0).WithMessage("ürün fiyatı 0'dan büyük olmalıdır.");
+        
+        // stock inclusiveBetween validation
+        RuleFor(x => x.Stock)
+            .InclusiveBetween(1, 100).WithMessage("stok adedi 1 ile 100 arasında olmalıdır");
+        
+        
+
+
+
+    }
+}
